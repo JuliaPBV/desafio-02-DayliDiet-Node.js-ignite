025262133e0f980b748ee39d3b20224074ab0216
JuliaPBV <vital.pereira.julia@gmail.com>
2025-05-07 07:52:40 -0300

    tratativas de variaveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify"
 import crypto from "node:crypto"
 import {knex} from "./database" 
+import { env } from "./env"
 
 const app = fastify() //base da aplicação
 
@@ -19,7 +20,7 @@ app.get('/hello', async () => {
 
 //oubir uma porta
 app.listen({ 
-    port: 3333,
+    port: env.PORT,
 }).then(() => {
     console.log('HTTP server running')
 })
